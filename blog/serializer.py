0ee20aec903cdadbbc930a0c2dd1829c4cdd0b02
Ryoichi Gokan <nargok@gmail.com>
2018-08-22 07:08:38 +0900

    Serializerの実装

---
+++ blog/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User, Entry
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ('name', 'mail')
+
+class EntorySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Entry
+    fields = ('title', 'body', 'created_at', 'status', 'author')
